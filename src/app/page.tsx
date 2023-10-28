729f7d6eca54d9d36de99aed41a36cf07ec6d7e3
owaiskreifeh <owais.abukreefeh@shahid.net>
2023-10-29 01:42:09 +0300

    feat: basic search with mode switching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 'use client'
-import { useCallback, useRef, useState } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import styles from './page.module.css'
 
 import SearchBar from '@/app/components/SearchBar/SearchBar'
 import { GitSearchResultPage } from '@/lib/GitSearchResultPage'
-import { searchByUsername } from './actions/gitActions'
 import { debounce } from '@/lib/utils/timing'
+
+import { searchByRepoName, searchByUsername } from './actions/gitActions'
 import { SearchResult } from './components/SearchResults/SearchResult'
 
 enum SearchMode {
@@ -33,35 +34,52 @@ export default function Home() {
   const lastSearchInput = useRef<string>("");
 
   const [searchMode, setSearchMode] = useState(SearchMode.USER);
-  const [searchResults, setSearchResults] = useState<GitSearchResultPage<any>[]>([])
+  const [searchResults, setSearchResults] = useState<Array<any>>([])
   const [currentPage, setCurrentPage] = useState(0);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string|null>(null)
+  const [error, setError] = useState<string | null>(null)
 
   const onSearchTextChanged = useCallback((value: string) => {
     if (!value) return; // NOOP
 
-
     // @TODO: recheck, some text may differ
     // if the current input is the same as the last input, or is starting substring of the previous one do nothing
-    if (lastSearchInput.current.startsWith(value)) {
-      return; // NOOP
-    }
+    // if (lastSearchInput.current.startsWith(value)) {
+    //   return; // NOOP
+    // }
 
     // update the last searched term
     lastSearchInput.current = value;
 
+    // Search
+
+    const searchMethod = searchMode == SearchMode.REPO ? searchByRepoName : searchByUsername;
     setLoading(true);
-    searchByUsername(value, 0 /** page */).then(data => { 
-      setSearchResults([GitSearchResultPage.deserialize(data)]);
-     }).catch((e) => {
+    searchMethod(value, 0 /** page */).then(data => {
+      setSearchResults(GitSearchResultPage.deserialize(data).searchResults);
+    }).catch((e) => {
       console.error(e);
       setError("something went wrong")
-     }).finally(() => {
+    }).finally(() => {
       setLoading(false)
-     })
+    })
 
-  }, [/** no dep */])
+  }, [searchMode])
+
+
+  useEffect(() => {
+    setCurrentPage(0);
+    setError(null);
+    onSearchTextChanged(lastSearchInput.current);
+  }, [onSearchTextChanged, searchMode])
+
+
+  function setMode(mode: SearchMode) {
+    setSearchResults([]);
+    setSearchMode(mode);
+  }
+
+
 
   return (
     <main className={styles.main}>
@@ -72,13 +90,32 @@ export default function Home() {
         onValidatorFail={console.error}
       />
 
-      {/* <SearchResult
+      <div className={styles.searchSelectionGroup}>
+        <span>
+          Search
+        </span>
+        <button
+          className={`${styles.button} ${searchMode == SearchMode.USER ? styles.active : null}`}
+          onClick={() => { setMode(SearchMode.USER) }}
+        >Users
+        </button>
+        <button
+          className={`${styles.button} ${searchMode == SearchMode.REPO ? styles.active : null}`}
+          onClick={() => { setSearchResults([]) ;setSearchMode(SearchMode.REPO) }}>
+          Repo
+        </button>
+        <span>
+          in GitHub
+        </span>
+      </div>
+
+      <SearchResult
         loading={loading}
-        renderComponent={renderComponent}
-        items={searchResults?.searchResults}
+        mode={searchMode}
+        items={searchResults}
         onReachEnd={() => setCurrentPage(currentPage + 1)}
-        /> */}
-      
+      />
+
     </main>
   )
 }
