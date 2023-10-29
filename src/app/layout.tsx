54ba8af379854570421c46b43c5994d1eda3cca8
owaiskreifeh <owais.abukreefeh@shahid.net>
2023-10-29 22:13:04 +0300

    fix: adding messing title and metadata to the html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>Gitsearch</title>
+        <meta name="description" content="GitSearch is a simple web application that allows users to search for GitHub users and view their repositories." />
+        <meta name="keywords" content="GitHub, Git, search, repository, user, web application" />
+        <meta name="author" content="Owais Kreifeh" />
+
+      </head>
       <storeContext.Provider value={store}>
         <body className={inter.className}>{children}</body>
       </storeContext.Provider>
